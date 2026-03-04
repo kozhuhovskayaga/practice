b679cbbfd0f8a5a7d8ae78f9c9251f54e75972f0
KozhuhovskayaGA <kozhuhovskayaga@mer.ci.nsu.ru>
2026-03-04 15:58:25 +0700

    Done composable

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,17 +1,41 @@
 package ci.nsu.moble.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ci.nsu.moble.main.ui.theme.Blue
+import ci.nsu.moble.main.ui.theme.DarkBlue
+import ci.nsu.moble.main.ui.theme.Green
+import ci.nsu.moble.main.ui.theme.Orange
 import ci.nsu.moble.main.ui.theme.PracticeTheme
+import ci.nsu.moble.main.ui.theme.Purple
+import ci.nsu.moble.main.ui.theme.Red
+import ci.nsu.moble.main.ui.theme.Yellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +43,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier) {paddingValues ->
+                    Column(
+                        modifier = Modifier.padding(paddingValues).
+                        fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Main()
+                    }
                 }
             }
         }
@@ -31,17 +58,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Main() {
+    val userColor = remember{mutableStateOf("")}
+    val label = remember{mutableStateOf("Применить цвет")}
+    val buttonColor = remember { mutableStateOf(Color.Black) }
+    val textButtonColor = remember { mutableStateOf(Color.White) }
+    val colorButtonCollection = mapOf(
+        "red" to Red,
+        "orange" to Orange,
+        "yellow" to Yellow,
+        "green" to Green,
+        "blue" to Blue,
+        "darkblue" to DarkBlue,
+        "purple" to Purple
     )
+
+    Column(modifier = Modifier.fillMaxWidth().
+    padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    )
+    {
+        TextField(userColor.value, onValueChange = {newText -> userColor.value = newText})
+        Button(onClick = {
+            buttonColor.value = colorButtonCollection.getOrDefault(userColor.value.lowercase(), Color.Gray)
+            if (!colorButtonCollection.containsKey(userColor.value.lowercase()))
+                Log.e("Main", "${userColor.value} not found")
+        },
+            Modifier.fillMaxWidth().padding(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = textButtonColor.value,
+                containerColor = buttonColor.value
+            ),
+            shape = RoundedCornerShape(10.dp))
+        {
+            Text(label.value)
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        for (color in colorButtonCollection)
+        {
+            Text (color.key.uppercase(),
+                modifier = Modifier.
+                drawBehind {
+                    drawRoundRect(
+                        color = color.value,
+                        cornerRadius = CornerRadius(10.dp.toPx())
+                    )
+                }.fillMaxWidth().padding(15.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     PracticeTheme {
-        Greeting("Android")
+        Main()
     }
 }
